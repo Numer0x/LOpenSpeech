0ac953d0c00a80d210061cf6072a0ee0bebeaf07
zac <sasha082700@icloud.com>
2024-09-02 17:06:57 +0800

    init

--- src/OpenSpeech.php
+++ src/OpenSpeech.php
@@ -28,6 +28,99 @@ class OpenSpeech
         ]
     ];
 
+    protected $timbre = [
+        'normalChinese' => [
+            '灿灿 2.0' => 'BV700_V2_streaming',
+            '炀炀' => 'BV705_streaming',
+            '擎苍 2.0' => 'BV701_V2_streaming',
+            '通用女声 2.0' => 'BV001_V2_streaming',
+            '灿灿' => 'BV700_streaming',
+            '超自然音色-梓梓2.0' => 'BV406_V2_streaming',
+            '超自然音色-梓梓' => 'BV406_streaming',
+            '超自然音色-燃燃2.0' => 'BV407_V2_streaming',
+            '超自然音色-燃燃' => 'BV407_streaming',
+            '通用女声' => 'BV001_streaming',
+            '通用男声' => 'BV002_streaming',
+            '知性姐姐-双语EDU' => 'BV034_streaming',
+            '温柔小哥EDU' => 'BV033_streaming',
+        ],
+        'normalAmericanEnglish' => [
+            '美式女声-Amelia' => 'BV027_streaming',
+            '讲述女声-Amanda' => 'BV502_streaming',
+            '活力女声-Ariana	' => 'BV503_streaming',
+            '活力男声-Jackson' => 'BV504_streaming',
+            '天才少女' => 'BV421_streaming',
+            'Stefan' => 'BV702_streaming',
+            '天真萌娃-Lily' => 'BV506_streaming',
+
+        ],
+        'normalBritishEnglish' => [
+            '亲切女声-Anna' => 'BV040_streaming',
+        ],
+        'normalAustralianEnglish' => [
+            '澳洲男声-Henry' => 'BV516_streaming',
+        ],
+        'normalJapanese' => [
+            '元气少女' => 'BV520_streaming',
+            '萌系少女' => 'BV521_streaming',
+            '天才少女' => 'BV421_streaming',
+            '气质女声' => 'BV522_streaming',
+            'Stefan' => 'BV702_streaming',
+            '灿灿' => 'BV700_streaming',
+            '日语男声' => 'BV524_streaming',
+        ],
+        'normalPortuguese' => [
+            '活力男声Carlos（巴西地区）' => 'BV531_streaming',
+            "活力女声（巴西地区）" => "BV530_streaming",
+            "天才少女" => "BV421_streaming",
+            "Stefan" => "BV702_streaming",
+            "灿灿" => "BV700_streaming"
+        ],
+        'normalSpanish' => [
+            "气质御姐（墨西哥地区）" => "BV065_streaming",
+            "天才少女" => "BV421_streaming",
+            "Stefan" => "BV702_streaming",
+            "灿灿" => "BV700_streaming"
+        ],
+        'normalThai' => [
+            '天才少女' => 'BV421_streaming'
+        ],
+        'normalVietnamese' => [
+            '天才少女' => 'BV421_streaming',
+        ],
+        'normalIndonesian' => [
+            "天才少女" => "BV421_streaming",
+            "Stefan" => "BV702_streaming",
+            "灿灿" => "BV700_streaming"
+        ],
+        'modelChinese' => [
+            "爽快思思/Skye" => "zh_female_shuangkuaisisi_moon_bigtts",
+            "温暖阿虎/Alvin" => "zh_male_wennuanahu_moon_bigtts",
+            "少年梓辛/Brayan" => "zh_male_shaonianzixin_moon_bigtts",
+            "邻家女孩" => "zh_female_linjianvhai_moon_bigtts",
+            "渊博小叔" => "zh_male_yuanboxiaoshu_moon_bigtts",
+            "阳光青年" => "zh_male_yangguangqingnian_moon_bigtts"
+        ],
+        'modelEnglish' => [
+            "爽快思思/Skye" => "zh_female_shuangkuaisisi_moon_bigtts",
+            "温暖阿虎/Alvin" => "zh_male_wennuanahu_moon_bigtts",
+            "少年梓辛/Brayan" => "zh_male_shaonianzixin_moon_bigtts",
+        ],
+        'modelJapanese' => [
+            "かずね（和音）/Javier or Álvaro" => "multi_male_jingqiangkanye_moon_bigtts",
+            "はるこ（晴子）/Esmeralda" => "multi_female_shuangkuaisisi_moon_bigtts",
+            "あけみ（朱美）" => "multi_female_gaolengyujie_moon_bigtts",
+            "ひろし（広志）/Roberto" => "multi_male_wanqudashu_moon_bigtts"
+        ],
+        'modelSpanish' => [
+            "かずね（和音）/Javier or Álvaro" => "multi_male_jingqiangkanye_moon_bigtts",
+            "はるこ（晴子）/Esmeralda" => "multi_female_shuangkuaisisi_moon_bigtts",
+            "ひろし（広志）/Roberto" => "multi_male_wanqudashu_moon_bigtts"
+        ]
+
+
+    ];
+
 
     public function __construct(Repository $config)
     {
@@ -136,6 +229,11 @@ class OpenSpeech
         fclose($myfile);
         return public_path('/audio/' . $fileName);
 
+    }
+
+    public function returnTimbreList()
+    {
+        return ['res' => $this->timbre, 'code' => 200, 'message' => 'Success'];
     }
 
 
